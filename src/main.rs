87c6a39c7c9359e40e20cbf3e865a62a67589776
Alex Ionitel <oranggsoda@gmail.com>
2023-02-27 18:35:51 -0500

    added bevy and just rendering blank screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::prelude::*;
+
+const SIZE: f32 = 640.0; // size of screen
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04))) // background color 
+		.add_plugins(DefaultPlugins.set(WindowPlugin {  // window config stuff
+			window: WindowDescriptor {
+				title: "Chess in Rust!".to_string(),
+				width: SIZE,
+				height: SIZE,
+				..Default::default()
+			},
+			..Default::default()
+		}))
+        .run();
 }
