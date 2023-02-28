404ae71b4bdf3c27c2925db48c135a1ae584e224
Alex Ionitel <oranggsoda@gmail.com>
2023-02-28 11:25:51 -0500

    board is spawning scaled to the screen size and in middle

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use bevy::prelude::*;
 
-const SIZE: f32 = 640.0; // size of screen
+const SIZE: f32 = 420.0; // size of screen (lol)
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04))) // background color 
 		.add_plugins(DefaultPlugins.set(WindowPlugin {  // window config stuff
 			window: WindowDescriptor {
 				title: "Chess in Rust!".to_string(),
@@ -21,20 +20,14 @@ fn main() {
 fn setup(
     mut commands: Commands, 
     asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>
 ) {
     // spawn camera entity
     commands.spawn(Camera2dBundle::default());
 
-    // slice sprite sheet using texture_handler to get just board
-    let texture_handle = asset_server.load("../assets/chess_assets.png"); // 400x248
-    let texture_atlas =
-    TextureAtlas::from_grid(texture_handle, Vec2::new(160.0, 126.0), 7, 1, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    // spawn chess entity
-    commands.spawn(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
+    // load board and spawn necessary sprite bundle for it
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("../assets/board.png"),
+        transform: Transform::from_scale(Vec3::new(3.25, 3.25, 1.)),
         ..default()
     });
 }
