99844ba794fa2f3e7d7780331ab3679559b6c5d1
Alex Ionitel <oranggsoda@gmail.com>
2023-02-27 20:33:56 -0500

    loading sprite sheet and slicing out board for now using texture_atlas

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,27 @@ fn main() {
 			},
 			..Default::default()
 		}))
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands, 
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>
+) {
+    // spawn camera entity
+    commands.spawn(Camera2dBundle::default());
+
+    // slice sprite sheet using texture_handler to get just board
+    let texture_handle = asset_server.load("../assets/chess_assets.png"); // 400x248
+    let texture_atlas =
+    TextureAtlas::from_grid(texture_handle, Vec2::new(160.0, 126.0), 7, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    // spawn chess entity
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
+        ..default()
+    });
 }
